2055c84dc91cb06660f23325f9515171eae8941e
Chirag Prajapati <chiragrprajapati28@gmail.com>
2021-02-13 14:21:28 +0530

    Build config and signing config added

---
+++ app/src/main/java/com/chichi/buildconfigvariable/App.kt
@@ -0,0 +1,6 @@
+package com.chichi.buildconfigvariable
+
+import android.app.Application
+
+class App:Application() {
+}
---
+++ app/src/main/java/com/chichi/buildconfigvariable/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.chichi.buildconfigvariable
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.textViewBaseUrl).text = BuildConfig.BASE_URL
+    }
+}
